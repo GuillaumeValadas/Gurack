3d18ffe9a640d5a56e3c686093a0b2d4c8acebe5
Guillaume Valadas <guillaume.valadas@gmail.com>
2018-01-30 14:39:11 +0000

    testing travis

--- tests/integrations/AuthentificationTest.php
+++ tests/integrations/AuthentificationTest.php
@@ -1,2 +1,15 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
+class AuthentificationTest extends	TestCase{
+
+	/** @test */
+	public function it_can_test()
+	{
+		$a = true;
+		$this->assertEquals(true,$a);
+
+	}
+
+}
